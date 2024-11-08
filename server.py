3f31223f7fe2ad2c54d445ae8d9ce114d67913dc
Nicole LeGare <legaren@immunant.com>
2024-11-08 15:34:24 -0800

    Setup previous and next buttons

--- server.py
+++ server.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# TODO: Figure this out at startup by scanning the contents of `static/comics`.
+NUM_COMICS = 11
+"""The total number of comics."""
+
 
 @app.route("/comic/<int:comic_id>")
 def comic(comic_id: int):
-    return render_template("comic.html", comic_id=comic_id)
+    return render_template("comic.html", comic_id=comic_id, num_comics=NUM_COMICS)
