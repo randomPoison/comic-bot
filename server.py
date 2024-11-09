4a8988139039073a8a1ac5e48e94d45f31f4821f
Nicole LeGare <legaren@immunant.com>
2024-11-09 12:03:22 -0800

    Setup base template and have comic template inherit from it

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ NUM_COMICS = 11
 
 @app.route("/comic/<int:comic_id>")
 def comic(comic_id: int):
-    return render_template("comic.html", comic_id=comic_id, num_comics=NUM_COMICS)
+    return render_template("comic.html.jinja", comic_id=comic_id, num_comics=NUM_COMICS)
 
 
 @app.route("/random")
